0e0fceb94e6c8a2593da3d1805e922106bdc6778
Michael Averitt <maveritt@mymail.tstc.edu>
2023-01-11 20:14:14 -0600

    Some progress bar work

--- MAverittACSM1/splashScreen.Designer.cs
+++ MAverittACSM1/splashScreen.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MAverittACSM1
 {
-    partial class Form1
+    partial class frmSplash
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,11 +29,42 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.tmrSplash = new System.Windows.Forms.Timer(this.components);
+            this.prgSplash = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // tmrSplash
+            // 
+            this.tmrSplash.Tick += new System.EventHandler(this.tmrSplash_Tick);
+            // 
+            // prgSplash
+            // 
+            this.prgSplash.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.prgSplash.Location = new System.Drawing.Point(124, 199);
+            this.prgSplash.Name = "prgSplash";
+            this.prgSplash.Size = new System.Drawing.Size(550, 45);
+            this.prgSplash.Step = 15;
+            this.prgSplash.TabIndex = 0;
+            // 
+            // frmSplash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::MAverittACSM1.Properties.Resources.splashScreen;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.prgSplash);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmSplash";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmSplash_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer tmrSplash;
+        private ProgressBar prgSplash;
     }
 }
--- MAverittACSM1/splashScreen.cs
+++ MAverittACSM1/splashScreen.cs
@@ -1,10 +1,47 @@
 namespace MAverittACSM1
 {
-    public partial class Form1 : Form
+    public partial class frmSplash : Form
     {
-        public Form1()
+        public int timeLeft { get; set; }
+        
+
+        public frmSplash()
         {
             InitializeComponent();
+        }
+
+        private void tmrSplash_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft> 0)
+            {
+                timeLeft = timeLeft - 1;
+            }
+            else
+            {
+                tmrSplash.Stop();
+                new frmMain().Show();
+                this.Hide();
+            }
+        }
+
+        private void frmSplash_Load(object sender, EventArgs e)
+        {
+            ProgressBar pBar = new ProgressBar();
+            pBar.Location = new System.Drawing.Point(125, 350);
+            pBar.Name = "prgSplash";
+            pBar.Width= 550;
+            pBar.Height= 45;
+            Controls.Add(pBar);
+            //pBar.Dock = DockStyle.Bottom;
+            pBar.Minimum = 0;
+            pBar.Maximum = 100;
+            pBar.Value= 0;
+
+
+            //pBar.Value= timeLeft;
+            timeLeft = 10;
+            tmrSplash.Start();
+            
         }
     }
 }
