56138f34e2602db817c16f9ae56697c370f5d211
Michael Averitt <maveritt@mymail.tstc.edu>
2023-01-14 17:40:56 -0600

    Splash screen comments

--- MAverittACSM1/splashScreen.cs
+++ MAverittACSM1/splashScreen.cs
@@ -12,6 +12,7 @@ namespace MAverittACSM1
 
         private void tmrSplash_Tick(object sender, EventArgs e)
         {
+            //Decrement timer, when 0 hide form, show main form, stop timer
             if (timeLeft > 0)
             {
                 timeLeft = timeLeft - 1;
@@ -26,14 +27,17 @@ namespace MAverittACSM1
 
         private void frmSplash_Load(object sender, EventArgs e)
         {
+            //Background worker for splash screen progress bar
             bkgWorker.WorkerReportsProgress= true;
             bkgWorker.RunWorkerAsync();
+            //Timer for splash screen, closes after 15 seconds
             timeLeft = 150;
             tmrSplash.Start();
         }
 
         private void bkgWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
         {
+            //Progress bar percentage counter
             for (int i = 1; i <= 100; i++)
             {
                 Thread.Sleep(150);
@@ -43,6 +47,7 @@ namespace MAverittACSM1
 
         private void bkgWorker_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
         {
+            //Progress bar percentage displays
             pgrSplash.Value = e.ProgressPercentage;
             lblProgress.Text = "Progress: " + e.ProgressPercentage.ToString() + "%";
         }
